1cf569a3a88e3419153744dbab8988be6ecae8f0
Gustavo <gutivalente@gmail.com>
2022-04-19 09:36:56 -0300

    CU-23z2rnd - Added docker to development (Windows)

--- docker/dev/run.sh
+++ docker/dev/run.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-
 export JEKYLL_VERSION=3.8
 
-docker run --rm \
+docker run --name saibrotech-site --rm \
   -p 4000:4000 \
   --volume="$PWD:/srv/jekyll" \
   -it jekyll/jekyll:$JEKYLL_VERSION \
